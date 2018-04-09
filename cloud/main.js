842ad8c4abf9404be2f60eb96afda2ffabf09cd4
MarioMartinezA <mariomartinez1@csumb.edu>
2018-04-09 16:29:45 -0700

    Changing usere type to host.

--- cloud/main.js
+++ cloud/main.js
@@ -3,11 +3,27 @@ Parse.Cloud.define('makeHost', function(request, response) {
 
 	var query = new Parse.Query(Parse.User);
 	query.equalTo("objectId", request.params.id);
-	
-	query.find({useMasterKey: true}).then(function(results) {
-		response.success("Found user");
-	}, function(error) {
-		response.error(error)
-	});
+
+	query.first({
+    useMasterKey: true,
+    success: function(Objects) {
+      Objects.save(null, {
+        useMasterKey: true,
+        success: function(user) {,
+          user.set("type","host");
+          user.save();
+          response.success('Cloud code: user is now a host');
+
+        },
+        error: function(error) {
+        	response.error(error);
+        }
+      });
+    },
+    error: function(error) {
+    	response.error(error);
+    }
+  });
+
  	//response.success('Hello, World! ' + request.params.username + ' ID: ' + request.params.id);
 });
