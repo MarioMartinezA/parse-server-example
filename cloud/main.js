9b2121f631bbd6bf95aaba50499f1fdb34f42af3
MarioMartinezA <mariomartinez1@csumb.edu>
2018-04-09 15:37:29 -0700

    Serching for user.

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,13 @@
 
 Parse.Cloud.define('makeHost', function(request, response) {
-  response.success('Hello, World! ' + request.params.username + ' ID: ' + request.params.id);
+
+	var query = new Parse.Query(Parse.User);
+	query.equalTo("objectId", request.params.id);
+	
+	query.find({useMasterKey: true}).then(function(results) {
+		response.success("Found user");
+	}, function(error) {
+		response.error(error)
+	});
+ 	//response.success('Hello, World! ' + request.params.username + ' ID: ' + request.params.id);
 });
