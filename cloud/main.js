47ad72ec7fa304e78ae4fa2c9f83b0747ad35705
MarioMartinezA <mariomartinez1@csumb.edu>
2018-04-09 17:24:34 -0700

    Cleaned code and added the function to host to user type.

--- cloud/main.js
+++ cloud/main.js
@@ -1,27 +1,50 @@
-
 Parse.Cloud.define('makeHost', function(request, response) {
 
 	var query = new Parse.Query(Parse.User);
 	query.equalTo("objectId", request.params.id);
 
 	query.first({
-    useMasterKey: true,
-    success: function(Objects) {
-    	Objects.set("type","host");
-     	Objects.save(null, {
-	        useMasterKey: true,
-	        success: function(user) {
-	        response.success('Cloud code: user is now a host');
-        },
-        error: function(error) {
-        	response.error(error);
-        }
-      });
-    },
-    error: function(error) {
-    	response.error(error);
-    }
-  });
+    	useMasterKey: true,
+    	success: function(Objects) {
+    		Objects.set("type","host");
+     		Objects.save(null, {
+	        	useMasterKey: true,
+	        	success: function(user) {
+	        		response.success('success');
+        		},
+		        error: function(error) {
+		        	response.error(error);
+		        }
+      		});
+	    },
+	    error: function(error) {
+	    	response.error(error);
+	    }
+  	});
+});
 
- 	//response.success('Hello, World! ' + request.params.username + ' ID: ' + request.params.id);
+
+Parse.Cloud.define('makeUser', function(request, response) {
+
+	var query = new Parse.Query(Parse.User);
+	query.equalTo("objectId", request.params.id);
+
+	query.first({
+    	useMasterKey: true,
+    	success: function(Objects) {
+    		Objects.set("type","user");
+     		Objects.save(null, {
+	        	useMasterKey: true,
+	        	success: function(user) {
+	        		response.success('success');
+        		},
+		        error: function(error) {
+		        	response.error(error);
+		        }
+      		});
+	    },
+	    error: function(error) {
+	    	response.error(error);
+	    }
+  	});
 });
